5f25404a5b68cdc356cbbbaba10149add823e080
Miguel A. Angulo <maangulo12@gmail.com>
2016-06-13 23:25:18 -0500

    Update manage

--- manage.py
+++ manage.py
@@ -4,23 +4,20 @@
     manage.py
     ~~~~~~~~~
 
-    This module is used for doing quick commands in this application.
+    This script is used for doing quick commands in this application.
 
-    -Commands:
-        create    : Creates all of the tables in the database.
-        drop      : Drops all of the tables from the database.
-        populate  : Populates the database with sample data.
-        recreate  : Drops, recreates, and populates the tables in the database.
-        runtests  : Runs tests to this application using nose.
-        db        : Performs database migrations.
-        shell     : Runs a Python shell using IPython.
+    Commands:
+    - create    : Creates all of the tables in the database.
+    - drop      : Drops all of the tables from the database.
+    - populate  : Populates the database with sample data.
+    - recreate  : Drops, recreates, and populates the tables in the database.
+    - runtests  : Runs tests to this application using nose.
+    - db        : Performs database migrations.
+    - shell     : Runs a Python shell using IPython.
 
-    -How to use this module (type the following in the command-line):
-        1. Type the following to see the available commands:
-            python3 manage.py
-
-        2. Or type the following to execute command:
-            python3 manage.py (insert command here)
+    HOW TO USE:
+    - Type the following in the command-line
+      python3 manage.py (insert command here)
 """
 
 import os
@@ -30,44 +27,49 @@ from app import manager, db
 from tests.sample_data import populate_db
 
 
-# Add db command for database migrations
-manager.add_command('db', MigrateCommand)
-
-
+# COMMAND: create
 @manager.command
 def create():
-    "Creates all of the tables in the database."
+    """Creates all of the tables in the database."""
     db.create_all()
     print('Created all of the tables in the database.')
 
 
+# COMMAND: drop
 @manager.command
 def drop():
-    "Drops all of the tables from the database."
+    """Drops all of the tables from the database."""
     db.drop_all()
     print('Dropped all of the tables from the database.')
 
 
+# COMMAND: populate
 @manager.command
 def populate():
-    "Populates the database with sample data."
+    """Populates the database with sample data."""
     populate_db()
     print('Populated the database with sample data.')
 
 
+# COMMAND: recreate
 @manager.command
 def recreate():
-    "Drops, recreates, and populates the tables in the database."
+    """Drops, recreates, and populates the tables in the database."""
     drop()
     create()
     populate()
 
 
+# COMMAND: runtests
 @manager.command
 def runtests():
-    "Runs tests to this application using nose."
+    """Runs tests to this application using nose."""
     os.system('nosetests tests/tests.py')
     print('Finished running all tests.')
+
+
+# COMMAND: db
+manager.add_command('db', MigrateCommand)
 
 
 if __name__ == "__main__":
