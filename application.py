77a03436aa9ce82ae48ce69bdf38ab25ab873c2d
Miguel A. Angulo <maangulo12@gmail.com>
2016-06-13 23:30:04 -0500

    Update docs

--- application.py
+++ application.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 """
     BuildersRecords API
-    ~~~~~~~~~~~~~~~~~~~
+    ~~~~~~~~~~~~~~~~~~~~
     :copyright: (c) 2016
 
-    Run this module to deploy the application.
+    This module runs the application.
 
-    -How to run it (type the following in the command-line):
-        python3 application.py
+    HOW TO USE:
+    - Type the following in the command-line
+      python3 application.py
 """
 
 from app import app
