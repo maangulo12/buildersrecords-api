3026bbf833213e38dc554f2bf7f720bc19f980ce
Miguel A. Angulo <maangulo12@gmail.com>
2016-06-17 15:00:16 -0500

    Update application

--- application.py
+++ application.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-    wsgi.py
-    ~~~~~~~
-
-    Run this module to run the application.
-
-    HOW TO USE:
-    - Type the following in the command-line
-      python3 wsgi.py
+    BuildersRecords API
+    ~~~~~~~~~~~~~~~~~~~~
+    :copyright: (c) 2016
 """
 
 from app import app
-
-
-if __name__ == '__main__':
-    app.run(host=app.config['SERVER_HOST'], port=app.config['SERVER_PORT'])
