22be5e0d3971a8ce0377df3da44b640f42ad151d
Miguel A. Angulo <maangulo12@gmail.com>
2016-06-21 14:07:12 -0500

    Add v1 package

--- app/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-    app.api package
-    ~~~~~~~~~~~~~~~~
-
-    This package contains the API endpoints of this application.
-"""
---
+++ app/api/v1/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+    app.api.v1 package
+    ~~~~~~~~~~~~~~~~~~
+
+    This package contains the API v1 endpoints of this application.
+"""
