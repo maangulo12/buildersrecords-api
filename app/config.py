67852d5377bd95d09ec5fa3c6791476ee8678470
Miguel A. Angulo <maangulo12@gmail.com>
2016-06-24 00:32:59 -0500

    Remove SERVER_HOST and SERVER_PORT

--- app/config.py
+++ app/config.py
@@ -14,17 +14,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'secret_key')
 DEBUG      = os.environ.get('DEBUG', True)
 TESTING    = os.environ.get('TESTING', True)
 
-# Server
-SERVER_HOST = os.environ.get('SERVER_HOST', '0.0.0.0')
-SERVER_PORT = os.environ.get('SERVER_PORT', 4444)
-
 # Email
-MAIL_SERVER         = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
-MAIL_PORT           = os.environ.get('MAIL_PORT', 465)
-MAIL_USE_SSL        = os.environ.get('MAIL_USE_SSL', True)
-MAIL_DEBUG          = os.environ.get('MAIL_DEBUG', False)
-MAIL_USERNAME       = os.environ.get('MAIL_USERNAME', 'buildersrecords.app@gmail.com')
-MAIL_PASSWORD       = os.environ.get('MAIL_PASSWORD' 'buildersrecords123')
+MAIL_SERVER         = os.environ.get('MAIL_SERVER', 'localhost')
+MAIL_PORT           = os.environ.get('MAIL_PORT', 25)
+MAIL_USE_SSL        = os.environ.get('MAIL_USE_SSL', False)
+MAIL_USERNAME       = os.environ.get('MAIL_USERNAME', 'email@address.com')
+MAIL_PASSWORD       = os.environ.get('MAIL_PASSWORD' 'password')
 MAIL_DEFAULT_SENDER = ('BuildersRecords', MAIL_USERNAME)
 
 # Database
